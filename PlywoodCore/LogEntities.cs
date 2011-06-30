f9fa58125dabf2318849a9751b6c852a7cbabb00
Daniel Bradley <daniel.dbphoto@gmail.com>
2011-06-30 23:40:34 +0100

    Converted log entries over to using XML serialisation including an XSD based validation.

--- PlywoodCore/LogEntities.cs
+++ PlywoodCore/LogEntities.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using Plywood.Utils;
 using System.IO;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Reflection;
+using System.Xml;
 
 namespace Plywood
 {
@@ -18,37 +22,35 @@ namespace Plywood
         }
 
         public LogEntry(string source)
-            : base()
-        {
-            Extend(LogEntry.Parse(source));
-        }
+            : this(LogEntry.Parse(source)) { }
 
         public LogEntry(Stream source)
-            : base()
-        {
-            Extend(LogEntry.Parse(source));
-        }
+            : this(LogEntry.Parse(source)) { }
 
         public LogEntry(TextReader source)
-            : base()
+            : this(LogEntry.Parse(source)) { }
+
+        public LogEntry(XmlTextReader source)
+            : this(LogEntry.Parse(source)) { }
+
+        public LogEntry(LogEntry prototype)
         {
-            Extend(LogEntry.Parse(source));
+            this.Timestamp = prototype.Timestamp;
+            this.Status = prototype.Status;
+            this.GroupKey = prototype.GroupKey;
+            this.TargetKey = prototype.TargetKey;
+            this.InstanceKey = prototype.InstanceKey;
+            this.LogContent = prototype.LogContent;
         }
 
         #endregion
 
         public DateTime Timestamp { get; set; }
         public LogStatus Status { get; set; }
+        public Guid GroupKey { get; set; }
+        public Guid TargetKey { get; set; }
         public Guid InstanceKey { get; set; }
         public string LogContent { get; set; }
-
-        private void Extend(LogEntry prototype)
-        {
-            this.Timestamp = prototype.Timestamp;
-            this.Status = prototype.Status;
-            this.InstanceKey = prototype.InstanceKey;
-            this.LogContent = prototype.LogContent;
-        }
 
         public Stream Serialise()
         {
@@ -62,15 +64,17 @@ namespace Plywood
             if (logEntry == null)
                 throw new ArgumentNullException("logEntry", "Version cannot be null.");
 
-            var values = new List<KeyValuePair<string, string>>()
-            {
-                new KeyValuePair<string,string>("Timestamp", Serialisation.SerialiseDateReversed(logEntry.Timestamp)),
-                new KeyValuePair<string,string>("Status", logEntry.Status.ToString()),
-                new KeyValuePair<string,string>("InstanceKey", logEntry.InstanceKey.ToString("N")),
-                new KeyValuePair<string,string>("LogContent", logEntry.LogContent),
-            };
+            var doc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XElement("logEntry",
+                    new XAttribute("timestamp", logEntry.Timestamp),
+                    new XAttribute("status", logEntry.Status),
+                    new XElement("groupKey", logEntry.GroupKey),
+                    new XElement("targetKey", logEntry.TargetKey),
+                    new XElement("instanceKey", logEntry.InstanceKey),
+                    new XElement("logContent", logEntry.LogContent)));
 
-            return Serialisation.Serialise(values);
+            return Serialisation.Serialise(doc);
         }
 
         public static LogEntry Parse(string source)
@@ -80,48 +84,92 @@ namespace Plywood
 
         public static LogEntry Parse(Stream source)
         {
-            return Parse(new StreamReader(source));
+            return Parse(new XmlTextReader(source));
         }
-
+        
         public static LogEntry Parse(TextReader source)
         {
-            var logEntry = new LogEntry();
-            var properties = Serialisation.ReadProperties(source);
+            return Parse(new XmlTextReader(source));
+        }
 
-            if (!properties.ContainsKey("Timestamp"))
-                throw new DeserialisationException("Failed deserialising instance: missing property \"Timestamp\"");
-            if (!properties.ContainsKey("Status"))
-                throw new DeserialisationException("Failed deserialising instance: missing property \"Status\"");
-            if (!properties.ContainsKey("InstanceKey"))
-                throw new DeserialisationException("Failed deserialising instance: missing property \"InstanceKey\"");
-            if (!properties.ContainsKey("LogContent"))
-                throw new DeserialisationException("Failed deserialising instance: missing property \"LogContent\"");
+        public static LogEntry Parse(XmlReader source)
+        {
+            XDocument doc;
+            try
+            {
+                doc = XDocument.Load(source);
+            }
+            catch (Exception ex)
+            {
+                throw new DeserialisationException("Failed deserialising log entry.", ex);
+            }
+
+            if (!ValidateInstanceXml(doc))
+                throw new DeserialisationException("Serialised log entry xml is not valid.");
 
             DateTime timestamp;
             LogStatus status;
-            Guid instanceKey;
+            Guid groupKey, targetKey, instanceKey;
 
-            try
+            if (!DateTime.TryParse(doc.Root.Attribute("timestamp").Value, out timestamp))
+                throw new DeserialisationException("Serialised log entry timestamp is not a valid datetime.");
+            if (!Enum.TryParse(doc.Root.Attribute("status").Value, out status))
+                throw new DeserialisationException("Serialised log entry status is not a valid log status.");
+            if (!Guid.TryParse(doc.Root.Element("groupKey").Value, out groupKey))
+                throw new DeserialisationException("Serialised log entry group key is not a valid guid.");
+            if (!Guid.TryParse(doc.Root.Element("targetKey").Value, out targetKey))
+                throw new DeserialisationException("Serialised log entry target key is not a valid guid.");
+            if (!Guid.TryParse(doc.Root.Element("instanceKey").Value, out instanceKey))
+                throw new DeserialisationException("Serialised log entry instance key is not a valid guid.");
+
+            var logEntry = new LogEntry()
             {
-                timestamp = Serialisation.DeserialiseReversedDate(properties["Timestamp"]);
-            }
-            catch (Exception)
-            {
-                throw new DeserialisationException("Failed deserialising version: invalid property value for \"Timestamp\"");
-            }
-
-            if (!Enum.TryParse<LogStatus>(properties["Status"], true, out status))
-                throw new DeserialisationException("Failed deserialising version: invalid property value for \"Status\"");
-            if (!Guid.TryParseExact(properties["InstanceKey"], "N", out instanceKey))
-                throw new DeserialisationException("Failed deserialising version: invalid property value for \"InstanceKey\"");
-
-            logEntry.Timestamp = timestamp;
-            logEntry.Status = status;
-            logEntry.InstanceKey = instanceKey;
-            logEntry.LogContent = properties["LogContent"];
+                Timestamp = timestamp.ToUniversalTime(),
+                Status = status,
+                GroupKey = groupKey,
+                TargetKey = targetKey,
+                InstanceKey = instanceKey,
+                LogContent = doc.Root.Element("logContent").Value,
+            };
 
             return logEntry;
         }
+
+        public static bool ValidateInstanceXml(XDocument targetDoc)
+        {
+            bool valid = true;
+            targetDoc.Validate(Schemas, (o, e) =>
+            {
+                valid = false;
+            });
+            return valid;
+        }
+
+        public static XmlSchemaSet Schemas
+        {
+            get
+            {
+                if (schemas == null)
+                {
+                    lock (schemasLock)
+                    {
+                        if (schemas == null)
+                        {
+                            var newSchemas = new XmlSchemaSet();
+                            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Plywood.Schemas.LogEntry.xsd"))
+                            {
+                                newSchemas.Add("", XmlReader.Create(stream));
+                            }
+                            schemas = newSchemas;
+                        }
+                    }
+                }
+                return schemas;
+            }
+        }
+
+        private static XmlSchemaSet schemas;
+        private static object schemasLock = new object();
 
         #endregion
 
@@ -143,10 +191,10 @@ namespace Plywood
     }
 
     public enum LogStatus
-	{
+    {
         Ok = 'a',
         Warning = 'b',
         Error = 'c',
         Fatal = 'd',
-	}
+    }
 }

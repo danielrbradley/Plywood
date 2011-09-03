8efb1c3e82f942829a60774833eb3322cad73f0f
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-09-03 01:29:08 +0100

    Created a new version tokeniser plus associated unit tests.

---
+++ PlywoodCore/Indexes/VersionTokeniser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plywood.Indexes
+{
+    public class VersionTokeniser : ITokeniser
+    {
+        public IEnumerable<string> Tokenise(string input)
+        {
+            return Enumerable.Range(1, input.Length).Select(n => input.Take(n).Select(c => c.ToString()).Aggregate((a, b) => a + b)).Where(t => !t.EndsWith("."));
+        }
+    }
+}
---
+++ PlywoodLibraryUnitTests/Indexes/VersionTokeniserTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plywood.Indexes;
+
+namespace Plywood.Tests.UnitTesting.Indexes
+{
+    [TestClass]
+    public class VersionTokeniserTests
+    {
+        [TestMethod]
+        public void TokeniseVersion1()
+        {
+            var version = "1";
+            var expected = new List<string>()
+            {
+                "1",
+            };
+            var actual = (new VersionTokeniser()).Tokenise(version);
+            CustomAsserts.AreCollectionsEqual<IEnumerable<string>, string>(expected, actual);
+        }
+
+        [TestMethod]
+        public void TokeniseVersion2()
+        {
+            var version = "1.0";
+            var expected = new List<string>()
+            {
+                "1",
+                "1.0",
+            };
+            var actual = (new VersionTokeniser()).Tokenise(version);
+            CustomAsserts.AreCollectionsEqual<IEnumerable<string>, string>(expected, actual);
+        }
+
+        [TestMethod]
+        public void TokeniseVersion3()
+        {
+            var version = "1.23.4";
+            var expected = new List<string>()
+            {
+                "1",
+                "1.2",
+                "1.23",
+                "1.23.4",
+            };
+            var actual = (new VersionTokeniser()).Tokenise(version);
+            CustomAsserts.AreCollectionsEqual<IEnumerable<string>, string>(expected, actual);
+        }
+    }
+}

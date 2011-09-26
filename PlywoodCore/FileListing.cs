1d2be5d8119f9cb03883c987759f6d6f07a11856
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-09-26 19:01:10 +0100

    Started created abstracted storage interface

---
+++ PlywoodCore/FileListing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plywood
+{
+    public class FileListing
+    {
+        public string FolderPath { get; set; }
+        public string Marker { get; set; }
+        public int PageSize { get; set; }
+        public IList<string> Items { get; set; }
+        public string NextMarker { get; set; }
+        public bool IsTruncated { get; set; }
+    }
+}
---
+++ PlywoodCore/IStorageProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Plywood
+{
+    public interface IStorageProvider
+    {
+        void DeleteFile(string path);
+        bool FileExists(string path);
+        Stream GetFile(string path);
+        void PutFile(string path, Stream content);
+        void MoveFolder(string oldFolderPath, string newFolderPath);
+        FileListing ListFiles(string folderPath, string marker, int pageSize);
+    }
+}

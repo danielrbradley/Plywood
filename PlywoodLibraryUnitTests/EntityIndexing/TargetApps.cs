fed8d26c65a88aca32fcc05b09121e66aebe27e9
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-11-02 11:25:53 +0000

    Made framework internals visible to unit test project to allow testing of target app index generation method.

--- PlywoodLibraryUnitTests/EntityIndexing/TargetApps.cs
+++ PlywoodLibraryUnitTests/EntityIndexing/TargetApps.cs
@@ -12,30 +12,27 @@ namespace Plywood.Tests.UnitTesting.EntityIndexing
         [TestMethod]
         public void GetTargetAppIndexPathsTestBasic()
         {
-            //var app = new RolePackage()
-            //{
-            //    RoleKey = new Guid("8d4abbb4-af9f-432f-bce5-e6da5a402469"),
-            //    RoleName = "Test Target",
-            //    PackageKey = new Guid("9a28d7be-d5b2-11e0-95ba-0c204924019b"),
-            //    PackageName = "Test App",
-            //    PackageDeploymentDirectory = "TestApp",
-            //};
+            var app = new RolePackage()
+            {
+                RoleKey = new Guid("8d4abbb4-af9f-432f-bce5-e6da5a402469"),
+                RoleName = "Test Target",
+                PackageKey = new Guid("9a28d7be-d5b2-11e0-95ba-0c204924019b"),
+                PackageName = "Test App",
+                PackageDeploymentDirectory = "TestApp",
+            };
 
-            //var expected = new List<string>()
-            //{
-            //    "t/8d4abbb4af9f432fbce5e6da5a402469/ai/e/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
-            //    "t/8d4abbb4af9f432fbce5e6da5a402469/ai/t/098f6bcd4621d373cade4e832627b4f6/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
-            //    "t/8d4abbb4af9f432fbce5e6da5a402469/ai/t/d2a57dc1d883fd21fb9951699df71cc7/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
-            //    "a/9a28d7bed5b211e095ba0c204924019b/ti/e/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
-            //    "a/9a28d7bed5b211e095ba0c204924019b/ti/t/098f6bcd4621d373cade4e832627b4f6/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
-            //    "a/9a28d7bed5b211e095ba0c204924019b/ti/t/42aefbae01d2dfd981f7da7d823d689e/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
-            //};
-            //var actual = app.GetIndexEntries();
+            var expected = new List<string>()
+            {
+                "r/8d4abbb4af9f432fbce5e6da5a402469/pi/e/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
+                "r/8d4abbb4af9f432fbce5e6da5a402469/pi/t/098f6bcd4621d373cade4e832627b4f6/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
+                "r/8d4abbb4af9f432fbce5e6da5a402469/pi/t/d2a57dc1d883fd21fb9951699df71cc7/testapp-9a28d7bed5b211e095ba0c204924019b-Test%20App-TestApp",
+                "p/9a28d7bed5b211e095ba0c204924019b/ri/e/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
+                "p/9a28d7bed5b211e095ba0c204924019b/ri/t/098f6bcd4621d373cade4e832627b4f6/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
+                "p/9a28d7bed5b211e095ba0c204924019b/ri/t/42aefbae01d2dfd981f7da7d823d689e/testtarget-8d4abbb4af9f432fbce5e6da5a402469-Test%20Target",
+            };
+            var actual = app.GetIndexEntries();
 
-            //CustomAsserts.AreCollectionsEqual<IEnumerable<string>, string>(expected, actual);
-
-            // TODO: Create accessor for the role package index entry generation tests
-            Assert.Inconclusive("TODO: Create accessor for the role package index entry generation tests");
+            CustomAsserts.AreCollectionsEqual<IEnumerable<string>, string>(expected, actual);
         }
     }
 }

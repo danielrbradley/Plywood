16c5fb9bd4ee976d3437cd7b2954e9603e8e4b3d
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-11-26 18:01:06 +0000

    Added group list documentation.

--- PlywoodFramework/GroupList.cs
+++ PlywoodFramework/GroupList.cs
@@ -1,23 +1,40 @@
 namespace Plywood
 {
-    using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
-    using System.Xml;
-    using System.Xml.Linq;
-    using System.Xml.Schema;
-    using Plywood.Indexes;
-    using Plywood.Utils;
 
+    /// <summary>
+    /// Represents a listing result for groups.
+    /// </summary>
     public class GroupList
     {
+        /// <summary>
+        /// Gets or sets the colleciton of group items returned.
+        /// </summary>
         public IEnumerable<GroupListItem> Groups { get; set; }
+
+        /// <summary>
+        /// Gets or sets the query used to retrieve the listing.
+        /// </summary>
         public string Query { get; set; }
+
+        /// <summary>
+        /// Gets or sets the marker used for the paging start position.
+        /// </summary>
         public string Marker { get; set; }
+
+        /// <summary>
+        /// Gets or sets the marker to get the next page of results.
+        /// </summary>
         public string NextMarker { get; set; }
+
+        /// <summary>
+        /// Gets or sets the page size specified for the listing.
+        /// </summary>
         public int PageSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the listing is truncated.
+        /// </summary>
         public bool IsTruncated { get; set; }
     }
 }

d415e3068ced4f502179f5e0c096d72ca6e2b381
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-11-26 15:15:25 +0000

    Setup stylecop and fixed the controller base warnings.

--- PlywoodFramework/ControllerBase.cs
+++ PlywoodFramework/ControllerBase.cs
@@ -1,25 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Configuration;
-
-namespace Plywood
+﻿namespace Plywood
 {
+    /// <summary>
+    /// Base class for all plywood framework controlers.
+    /// </summary>
     public abstract class ControllerBase
     {
+        /// <summary>
+        /// Storage provider to be used for the lifetime of the controller.
+        /// </summary>
         private IStorageProvider storageProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the ControllerBase class with a storage provider.
+        /// </summary>
+        /// <param name="storageProvider">Storage provider for the controller to use.</param>
+        public ControllerBase(IStorageProvider storageProvider)
+        {
+            this.storageProvider = storageProvider;
+        }
+
+        /// <summary>
+        /// Gets the storage provider being used by the current instance of the controller.
+        /// </summary>
         public IStorageProvider StorageProvider
         {
             get
             {
-                return storageProvider;
+                return this.storageProvider;
             }
-        }
-
-        public ControllerBase(IStorageProvider storageProvider)
-        {
-            this.storageProvider = storageProvider;
         }
     }
 }

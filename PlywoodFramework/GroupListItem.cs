217b9a6a75b3cf45a963074cf40093a750da8f1f
daniel.bradley <daniel.bradley@adfero.co.uk>
2011-11-26 18:07:07 +0000

    Added group list item documentation.

--- PlywoodFramework/GroupListItem.cs
+++ PlywoodFramework/GroupListItem.cs
@@ -1,35 +1,49 @@
 namespace Plywood
 {
     using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
-    using System.Xml;
-    using System.Xml.Linq;
-    using System.Xml.Schema;
-    using Plywood.Indexes;
-    using Plywood.Utils;
 
+    /// <summary>
+    /// Represents a single group result returned in a group list.
+    /// </summary>
     public class GroupListItem
     {
+        /// <summary>
+        /// Initializes a new instance of the GroupListItem class.
+        /// </summary>
         public GroupListItem()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the GroupListItem class from a path.
+        /// </summary>
+        /// <param name="path">Path of the index entry of the result item.</param>
         public GroupListItem(string path)
         {
             var segments = Utils.Indexes.GetIndexFileNameSegments(path);
             if (segments.Length != 3)
+            {
                 throw new ArgumentException("A group path index entry does not contain exactly 3 segments.", "path");
+            }
 
-            Marker = Utils.Indexes.GetIndexFileName(path);
-            Key = Utils.Indexes.DecodeGuid(segments[1]);
-            Name = Utils.Indexes.DecodeText(segments[2]);
+            this.Marker = Utils.Indexes.GetIndexFileName(path);
+            this.Key = Utils.Indexes.DecodeGuid(segments[1]);
+            this.Name = Utils.Indexes.DecodeText(segments[2]);
         }
 
-        internal string Marker { get; set; }
+        /// <summary>
+        /// Gets or sets the key identifier of the group.
+        /// </summary>
         public Guid Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the group.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the marker of this item.
+        /// </summary>
+        internal string Marker { get; set; }
     }
 }

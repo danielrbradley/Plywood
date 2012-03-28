02651844b94d25ded43074a7e5aaa03c10831cec
daniel <daniel@danielbradley.net>
2012-03-28 17:49:50 +0100

    Implemented remaining serialisation methods for the context entity.

--- Framework/Context.cs
+++ Framework/Context.cs
@@ -5,11 +5,43 @@ using System.Text;
 using System.Security.Cryptography;
 using System.IO;
 using Plywood.Indexes;
+using System.Xml.Schema;
+using System.Xml;
+using System.Reflection;
+using System.Xml.Linq;
+using Plywood.Utils;
 
 namespace Plywood
 {
     public class Context : IIndexableEntity
     {
+        #region Constructors
+
+        public Context()
+        {
+            this.Tags = new Dictionary<string, string>();
+        }
+
+        public Context(string source)
+            : this(Context.Parse(source)) { }
+
+        public Context(Stream source)
+            : this(Context.Parse(source)) { }
+
+        public Context(TextReader source)
+            : this(Context.Parse(source)) { }
+
+        public Context(XmlTextReader source)
+            : this(Context.Parse(source)) { }
+
+        public Context(Context other)
+        {
+            this.Name = other.Name;
+            this.Tags = other.Tags;
+        }
+
+        #endregion
+
         public Guid Key
         {
             get
@@ -22,12 +54,6 @@ namespace Plywood
         public ContextHierarchy Hierarchy { get { return new ContextHierarchy(this.Name); } }
         public Dictionary<string, string> Tags { get; set; }
 
-        /// <example>
-        /// Some.Context.Path
-        /// -> /ci/../index-entry
-        ///    /c/{Some}/ci/../index-entry
-        ///    /c/{Some.Context}/ci/../index-entry
-        /// </example>
         public IEnumerable<string> GetIndexEntries()
         {
             var indexKey = string.Format(
@@ -64,5 +90,124 @@ namespace Plywood
                 currentHierarchy = currentHierarchy.Parent;
             }
         }
+
+        public Stream Serialise()
+        {
+            return Context.Serialise(this);
+        }
+
+        #region Static Serialisation Methods
+
+        public static Stream Serialise(Context context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context", "Context cannot be null.");
+            if (!Validation.IsNameValid(context.Name))
+                throw new ArgumentException("Name must be valid (not blank & only a single line).");
+
+            var doc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XElement("context",
+                    new XElement("name", context.Name),
+                    new XElement("tags")));
+
+            if (context.Tags != null && context.Tags.Count > 0)
+            {
+                doc.Root.Element("tags").Add(
+                    context.Tags.Select(t =>
+                        new XElement("tag",
+                            new XAttribute("key", t.Key),
+                            t.Value
+                            )));
+            }
+
+            return Serialisation.Serialise(doc);
+        }
+
+        public static Context Parse(string source)
+        {
+            return Parse(new StringReader(source));
+        }
+
+        public static Context Parse(Stream source)
+        {
+            return Parse(new XmlTextReader(source));
+        }
+
+        public static Context Parse(TextReader source)
+        {
+            return Parse(new XmlTextReader(source));
+        }
+
+        public static Context Parse(XmlReader source)
+        {
+            XDocument doc;
+            try
+            {
+                doc = XDocument.Load(source);
+            }
+            catch (Exception ex)
+            {
+                throw new DeserialisationException("Failed deserialising context.", ex);
+            }
+
+            if (!ValidateContextXml(doc))
+                throw new DeserialisationException("Serialised context xml is not valid.");
+
+            var context = new Context()
+            {
+                Name = doc.Root.Element("name").Value,
+            };
+
+            var tagsElement = doc.Root.Element("tags");
+            if (tagsElement != null && tagsElement.HasElements)
+            {
+                context.Tags = tagsElement.Elements("tag").ToDictionary(t => t.Attribute("key").Value, t => t.Value);
+            }
+            else
+            {
+                context.Tags = new Dictionary<string, string>();
+            }
+
+            return context;
+        }
+
+        public static bool ValidateContextXml(XDocument contextDoc)
+        {
+            bool valid = true;
+            contextDoc.Validate(Schemas, (o, e) =>
+            {
+                valid = false;
+            });
+            return valid;
+        }
+
+        public static XmlSchemaSet Schemas
+        {
+            get
+            {
+                if (schemas == null)
+                {
+                    lock (schemasLock)
+                    {
+                        if (schemas == null)
+                        {
+                            var newSchemas = new XmlSchemaSet();
+                            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Plywood.Schemas.Context.xsd"))
+                            {
+                                newSchemas.Add("", XmlReader.Create(stream));
+                            }
+                            schemas = newSchemas;
+                        }
+                    }
+                }
+                return schemas;
+            }
+        }
+
+        private static XmlSchemaSet schemas;
+        private static object schemasLock = new object();
+
+        #endregion
     }
 }
---
+++ Tests.Framework.Unit/Entities/ContextTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Plywood.Tests.Framework.Unit.Entities
+{
+    /// <summary>
+    /// Summary description for AppTests
+    /// </summary>
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void ContextSerialiseDeserialise()
+        {
+            var originalContext = new Context()
+            {
+                Name = "Test Context",
+                Tags = new Dictionary<string, string>()
+                {
+                    { "tagKey", "Some tag value." },
+                    { "secondKey", "Multiline \r\n   test!" }
+                }
+            };
+
+            Context secondContext;
+            using (var stream = originalContext.Serialise())
+            {
+                secondContext = new Context(stream);
+            }
+
+            Assert.AreEqual(originalContext.Key, secondContext.Key);
+            Assert.AreEqual(originalContext.Name, secondContext.Name);
+
+            Assert.IsNotNull(secondContext.Tags);
+            foreach (var tag in originalContext.Tags)
+            {
+                Assert.IsTrue(secondContext.Tags.ContainsKey(tag.Key));
+                Assert.AreEqual(tag.Value, secondContext.Tags[tag.Key]);
+            }
+        }
+    }
+}
